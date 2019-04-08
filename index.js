e9b1a490abdbb7a7702e60b9ab9576b436fe274d
KingOfTheBENDER <43980314+KingOfTheBENDER@users.noreply.github.com>
2019-04-08 19:34:32 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * @format
+ */
+
+import {AppRegistry,UIManager} from 'react-native';
+import App from './src/app';
+
+UIManager . setLayoutAnimationEnabledExperimental  &&
+UIManager . setLayoutAnimationEnabledExperimental ( true );
+
+AppRegistry.registerComponent('tech_stack', () => App);
